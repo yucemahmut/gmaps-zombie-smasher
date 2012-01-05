77d4a005f3b15e13267d377a59af51cac84dab96
brice.nedelec@gmail.com <brice.nedelec@gmail.com@18530f2c-7227-f937-7b60-d945714497b6>
2012-01-05 21:29:22 +0000

    Ajout de quelques algorithmes

--- trunk/src/fr/alma/ihm/gmapszombiesmasher/model/components/CAIZombie.java
+++ trunk/src/fr/alma/ihm/gmapszombiesmasher/model/components/CAIZombie.java
@@ -2,13 +2,13 @@ package fr.alma.ihm.gmapszombiesmasher.model.components;
 
 import fr.alma.ihm.gmapszombiesmasher.model.Entity;
 
-/** 
+/**
  * 
- * Artificial intelligence of an Entity if it do have one.
- * Zombie will move to the nearest citizen in order to eat him
- *
+ * Artificial intelligence of an Entity if it do have one. Zombie will move to
+ * the nearest citizen in order to eat him
+ * 
  */
-public class CAIZombie extends Component implements ICAI{
+public class CAIZombie extends Component implements ICAI {
 
 	public CAIZombie(Entity parent) {
 		super(parent);
@@ -17,7 +17,23 @@ public class CAIZombie extends Component implements ICAI{
 	@Override
 	public void update() {
 		// TODO Auto-generated method stub
-		
+
+		// Chercher dans le parent si il a un CGoal
+		// Si ce n'est pas le cas , alors le créer
+
+		// Si c'est le cas, alors
+		// Si le but est null, alors trouver une cible appropriée
+
+		// Sinon
+		// Signifie qu'un but est déjà fixé
+
+		// CONSTANTE % de chances de prendre une cible aléatoire ( 1 a 10 % en
+		// fonction du nombre de zombies )
+
+		// Par effet de bord, changer les variables de CCoordinates en fonction
+		// de la CMoveSpeed (ne pas oublier deltaTime => demander a Adrien pour
+		// plus de précisions) et de la route trouvée par Gmaps
+
 	}
 
 }
