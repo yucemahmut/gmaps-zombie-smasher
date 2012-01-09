624d5a716cd0f1f35f9422d7f077df04d6cfab4a
brice.nedelec@gmail.com <brice.nedelec@gmail.com@18530f2c-7227-f937-7b60-d945714497b6>
2012-01-09 14:54:33 +0000

    Avancée sur les AI

--- trunk/src/fr/alma/ihm/gmapszombiesmasher/model/components/CAIZombie.java
+++ trunk/src/fr/alma/ihm/gmapszombiesmasher/model/components/CAIZombie.java
@@ -1,5 +1,8 @@
 package fr.alma.ihm.gmapszombiesmasher.model.components;
 
+import java.util.LinkedList;
+import java.util.Random;
+
 import fr.alma.ihm.gmapszombiesmasher.model.Entity;
 
 /**
@@ -9,6 +12,9 @@ import fr.alma.ihm.gmapszombiesmasher.model.Entity;
  * 
  */
 public class CAIZombie extends Component implements ICAI {
+
+	private static int RANDOMBOUNDARY = 5; // bound of randomize a new target
+	private LinkedList<Entity> citizens;
 
 	public CAIZombie(Entity parent) {
 		super(parent);
@@ -20,26 +26,54 @@ public class CAIZombie extends Component implements ICAI {
 		// TODO fonction de recherche d'entité citizen plus proche
 		// Chercher dans le parent si il a un CGoal
 		// Si ce n'est pas le cas , alors le créer
+		if (!(getParent().getComponentMap().containsKey("CGoal"))) {
+			getParent().addComponent(new CGoal(getParent()));
+		}
 
 		// Si c'est le cas, alors
-		// Si le but est null, alors trouver une cible appropriée
+		// Si le but est null
+		CGoal g = (CGoal) getParent().getComponentMap().get("CGoal");
 
-		// Sinon
-		// Signifie qu'un but est déjà fixé
+		if (citizens.size() > 0) { // si citoyen il y a
+			Random random = new Random();
+			int percent = random.nextInt(100);
 
-		// CONSTANTE % de chances de prendre une cible aléatoire ( 1 a 10 % en
-		// fonction du nombre de zombies )
+			if (percent < this.RANDOMBOUNDARY) {
+				// Getting a random citizen for target
+				g.setGoal(citizens.get(random.nextInt()));
+			} else {
 
-		// Par effet de bord, changer les variables de CCoordinates en fonction
-		// de la CMoveSpeed (ne pas oublier deltaTime => demander a Adrien pour
-		// plus de précisions) et de la route trouvée par Gmaps ( que l on
-		// conserve en mémoire au cas où la position
-		// de l helico ne change pas a la frame suivante )
-		
+				// TODO calcul des distances
+
+			}
+		} else {
+			g.setGoal(null);
+		}
+
+		if (g.getGoal() != null) { // Une entité est ciblée
+			// Par effet de bord, changer les variables de CCoordinates en
+			// fonction
+			// de la CMoveSpeed (ne pas oublier deltaTime => demander a Adrien
+			// pour
+			// plus de précisions) et de la route trouvée par Gmaps ( que l on
+			// conserve en mémoire au cas où la position
+			// de l citizen ne change pas a la frame suivante )
+
+		}
+
+		// utilisation des distances calculé précedement pour la
 		// Verification de proximité
 		// Si un citizen se trouve proche, il est transformé en zombi, son icone
 		// devient celle d ' un zombi et son IA aussi, movespeed recalculé
 
+	}
+
+	public LinkedList<Entity> getCitizens() {
+		return citizens;
+	}
+
+	public void setCitizens(LinkedList<Entity> citizens) {
+		this.citizens = citizens;
 	}
 
 }
