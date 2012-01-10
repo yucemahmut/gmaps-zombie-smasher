1be04b4d6f8cafef9ed63e2b1f1e6270f80678b8
bowengao3@gmail.com <bowengao3@gmail.com@18530f2c-7227-f937-7b60-d945714497b6>
2012-01-10 01:20:36 +0000

    create new package for sound management and made a sound test with play button

--- trunk/src/fr/alma/ihm/gmapszombiesmasher/listeners/PlayButtonListener.java
+++ trunk/src/fr/alma/ihm/gmapszombiesmasher/listeners/PlayButtonListener.java
@@ -1,23 +1,31 @@
 package fr.alma.ihm.gmapszombiesmasher.listeners;
 
+
 import android.app.Activity;
 import android.content.Intent;
 import android.view.View;
 import android.view.View.OnClickListener;
 import fr.alma.ihm.gmapszombiesmasher.PlayActivity;
+import fr.alma.ihm.gmapszombiesmasher.sounds.SoundsManager;
+import fr.alma.ihm.gmapszombiesmasher.sounds.SoundsManagerFactory;
 
 public class PlayButtonListener implements OnClickListener {
 
 	private Activity parent;
+	private SoundsManager soundManager;
 
 	public PlayButtonListener(Activity activity) {
 		this.parent = activity;
+		soundManager = SoundsManagerFactory.get(activity);
 	}
 
 	@Override
 	public void onClick(View v) {
 		Intent intent = new Intent().setClass(this.parent, PlayActivity.class);
-	    this.parent.startActivity(intent);
+        this.parent.startActivity(intent);
+		
+        soundManager.playSound(SoundsManager.EXPLOSION);
+
 	}
 
 }
