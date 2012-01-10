80aa66e9e39e01b4de7450bbf615a39e876d1a87
adrien.bougouin <adrien.bougouin@18530f2c-7227-f937-7b60-d945714497b6>
2012-01-10 21:59:10 +0000

    distance calcul between two coordinates (one of my old wikipedia implementation)

--- trunk/src/fr/alma/ihm/gmapszombiesmasher/model/components/CCoordinates.java
+++ trunk/src/fr/alma/ihm/gmapszombiesmasher/model/components/CCoordinates.java
@@ -34,4 +34,23 @@ public class CCoordinates extends Component {
 	public void setLongitude(int longitude) {
 		this.longitude = longitude;
 	}
+
+  // http://fr.wikipedia.org/wiki/Calcul_distance_ente_deux_coordonnées_géographiques
+  public double distanceTo(CCoordinates c) {
+    double r = 6378.137;
+    double lat1 = (double) latitude / 1000000.0;
+    double lon1 = (double) longitude / 1000000.0;
+    double lat2 = (double) c.latitude / 1000000.0;
+    double lon2 = (double) c.longitude / 1000000.0;
+
+    double tetaLat = lat2 - lat1;
+    double tetaLon = lon2 - lon1;
+
+    double deltaLat = tetaLat * r * (Math.PI / 180);
+    double deltaLon = tetaLon * r * Math.cos(tetaLat) * (Math.PI / 180);
+
+    double d = (Math.sqrt(Math.pow(deltaLat, 2) + Math.pow(deltaLon, 2))) * 1000;
+
+    return d;
+  }
 }
