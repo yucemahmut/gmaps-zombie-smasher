cd8081e14b48b1744e2de428c9f5397d76702523
alban.menager@gmail.com <alban.menager@gmail.com@18530f2c-7227-f937-7b60-d945714497b6>
2012-01-12 00:02:23 +0000

    Can now set satellite mode in and out game (same activity for settings in main than settings in game menu)

--- trunk/src/fr/alma/ihm/gmapszombiesmasher/SettingPreferenceActivity.java
+++ trunk/src/fr/alma/ihm/gmapszombiesmasher/SettingPreferenceActivity.java
@@ -4,9 +4,18 @@ import android.os.Bundle;
 import android.preference.PreferenceActivity;
 
 public class SettingPreferenceActivity extends PreferenceActivity {
+	public static final String PREFS_NAME = "MyPrefsFile";
+	public static final String SATELLITE_VIEW_IN_MAP = "check_satellite";
+	public static final String APPLICATION_SOUNDS = "check_sound";
+	
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         addPreferencesFromResource(R.layout.setting_preference);        
-    }   
+    }
+    
+    protected void onStop (){
+    	super.onStop();
+    	setResult(RESULT_OK);
+    }
 }
