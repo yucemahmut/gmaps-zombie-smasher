678bdd61c0afe9c52da50272e54ff495c6a01a61
alban.menager@gmail.com <alban.menager@gmail.com@18530f2c-7227-f937-7b60-d945714497b6>
2012-01-15 19:23:39 +0000

    Add internet connection test and better gestion of spawn and dificulty level

--- src/fr/alma/ihm/gmapszombiesmasher/SettingPreferenceActivity.java
+++ src/fr/alma/ihm/gmapszombiesmasher/SettingPreferenceActivity.java
@@ -7,6 +7,7 @@ public class SettingPreferenceActivity extends PreferenceActivity {
 	public static final String PREFS_NAME = "MyPrefsFile";
 	public static final String SATELLITE_VIEW_IN_MAP = "check_satellite";
 	public static final String APPLICATION_SOUNDS = "check_sound";
+	public static final String DIFICULTY_LEVEL = "level";
 	
     @Override
     public void onCreate(Bundle savedInstanceState) {
