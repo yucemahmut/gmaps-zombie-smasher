80c2b19a422c465061c1b651de04e945686f642d
bowengao3@gmail.com <bowengao3@gmail.com@18530f2c-7227-f937-7b60-d945714497b6>
2012-01-16 16:18:11 +0000

    set some preference listeners for preference activity

--- src/fr/alma/ihm/gmapszombiesmasher/SettingPreferenceActivity.java
+++ src/fr/alma/ihm/gmapszombiesmasher/SettingPreferenceActivity.java
@@ -1,7 +1,14 @@
 package fr.alma.ihm.gmapszombiesmasher;
 
+import android.content.Context;
+import android.media.AudioManager;
 import android.os.Bundle;
+import android.preference.CheckBoxPreference;
+import android.preference.ListPreference;
+import android.preference.Preference;
 import android.preference.PreferenceActivity;
+import android.preference.Preference.OnPreferenceChangeListener;
+import android.widget.Toast;
 
 public class SettingPreferenceActivity extends PreferenceActivity {
 	public static final String PREFS_NAME = "MyPrefsFile";
@@ -9,14 +16,61 @@ public class SettingPreferenceActivity extends PreferenceActivity {
 	public static final String APPLICATION_SOUNDS = "check_sound";
 	public static final String DIFICULTY_LEVEL = "level";
 	
+	private Context context;
+	
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        addPreferencesFromResource(R.layout.setting_preference);        
+        addPreferencesFromResource(R.layout.setting_preference);   
+        context = this;
+        
+        // sound checkbox action
+        CheckBoxPreference soundBox = (CheckBoxPreference) findPreference(APPLICATION_SOUNDS);
+        soundBox.setOnPreferenceChangeListener(new OnPreferenceChangeListener() {  
+        	@Override
+        	public boolean onPreferenceChange(Preference arg0, Object newValue) {
+        		Toast.makeText(context, "Sound " +  ((Boolean)newValue?"On":"Off"), Toast.LENGTH_LONG).show();
+        		
+        		// to do: sound control method
+        		// ...        			
+        		
+        		return true;
+        	}
+        } );
+        
+        // satelitte checkbox action
+        CheckBoxPreference satelliteBox = (CheckBoxPreference) findPreference(SATELLITE_VIEW_IN_MAP);
+        satelliteBox.setOnPreferenceChangeListener(new OnPreferenceChangeListener() {  
+        	@Override
+        	public boolean onPreferenceChange(Preference arg0, Object newValue) {
+        		Toast.makeText(context, "Satelitte " +  ((Boolean)newValue?"On":"Off"), Toast.LENGTH_LONG).show();
+        		
+        		// to do: satelitte control method
+        		// ...
+        		
+        		return true;
+        	}
+        } );
+        
+        // level action
+        ListPreference levelList = (ListPreference) findPreference(DIFICULTY_LEVEL);
+        levelList.setOnPreferenceChangeListener(new OnPreferenceChangeListener() {  
+        	@Override
+        	public boolean onPreferenceChange(Preference arg0, Object newValue) {
+        		Toast.makeText(context, "Level " +  newValue + " choosed", Toast.LENGTH_LONG).show();
+        		
+        		// to do: level control method
+        		// ...
+        		
+        		return true;
+        	}
+        } );
     }
     
     protected void onStop (){
     	super.onStop();
     	setResult(RESULT_OK);
     }
+    
+
 }
