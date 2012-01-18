d978f04318837fa960df929be65cc1622ae403da
bowengao3@gmail.com <bowengao3@gmail.com@18530f2c-7227-f937-7b60-d945714497b6>
2012-01-18 01:55:29 +0000

    issue1 fixed, thanks for mention : )

--- src/fr/alma/ihm/gmapszombiesmasher/listeners/PlayButtonListener.java
+++ src/fr/alma/ihm/gmapszombiesmasher/listeners/PlayButtonListener.java
@@ -6,6 +6,7 @@ import android.content.Intent;
 import android.view.View;
 import android.view.View.OnClickListener;
 import fr.alma.ihm.gmapszombiesmasher.PlayActivity;
+import fr.alma.ihm.gmapszombiesmasher.sounds.BackgroundMusicService;
 import fr.alma.ihm.gmapszombiesmasher.sounds.SoundsManager;
 import fr.alma.ihm.gmapszombiesmasher.sounds.SoundsManagerFactory;
 
@@ -23,7 +24,7 @@ public class PlayButtonListener implements OnClickListener {
 	public void onClick(View v) {
 		Intent intent = new Intent().setClass(this.parent, PlayActivity.class);
         this.parent.startActivity(intent);
-		
+        parent.stopService(new Intent(parent, BackgroundMusicService.class));
         soundManager.playSound(SoundsManager.EXPLOSION);
 
 	}
