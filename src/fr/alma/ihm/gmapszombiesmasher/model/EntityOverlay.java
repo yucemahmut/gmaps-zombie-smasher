5f2fd274876cb0d985ed79e26b1d8a9f44dd311f
alban.menager@gmail.com <alban.menager@gmail.com@18530f2c-7227-f937-7b60-d945714497b6>
2012-01-22 16:22:04 +0000

    Remove shadow

--- src/fr/alma/ihm/gmapszombiesmasher/model/EntityOverlay.java
+++ src/fr/alma/ihm/gmapszombiesmasher/model/EntityOverlay.java
@@ -2,21 +2,25 @@ package fr.alma.ihm.gmapszombiesmasher.model;
 
 import java.util.ArrayList;
 
+import android.graphics.Canvas;
 import android.graphics.drawable.Drawable;
 
 import com.google.android.maps.ItemizedOverlay;
+import com.google.android.maps.MapView;
 import com.google.android.maps.OverlayItem;
 
 public class EntityOverlay extends ItemizedOverlay<OverlayItem> {
 	
 	private ArrayList<OverlayItem> mOverlays = new ArrayList<OverlayItem>();
+	private boolean shadow;
 	
 	public EntityOverlay(Drawable defaultMarker) {
 		super(boundCenterBottom(defaultMarker));
 	}
 	
-	public void addOverlay(OverlayItem overlay) {
+	public void addOverlay(OverlayItem overlay, boolean shadow) {
 	    mOverlays.add(overlay);
+	    this.shadow = shadow;
 	    populate();
 	}
 	
@@ -31,4 +35,10 @@ public class EntityOverlay extends ItemizedOverlay<OverlayItem> {
 		return mOverlays.size();
 	}
 
+	@Override
+    public void draw(Canvas canvas, MapView mapView, boolean shadow)
+    {
+		if(!shadow)
+			super.draw(canvas, mapView, false);
+    }
 }
